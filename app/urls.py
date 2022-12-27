513e2b58abe326bdf741bfb9428820c20536f041
Michal <michal@Michals-MacBook-Pro.local>
2022-12-27 12:12:20 +0100

    update urls

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from app.resources import Metrics, Order
 
 api = Api(prefix="/orders")
 
-api.add_resource(Order, "/<int:order_id>", "")
+api.add_resource(Order, "/<int:order_id>", "/", "")
 api.add_resource(Metrics, "/metrics")
