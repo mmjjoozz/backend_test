b13058ec7a1ab0c0ed53d7d10c7a55c6794e618c
Michal <michal@Michals-MacBook-Pro.local>
2022-12-27 10:56:50 +0100

    add database and pre-populate data

--- app/api.py
+++ app/api.py
@@ -16,6 +16,17 @@ def create_app(config=DevConfig):
     with app.app_context():
         db.init_app(app)
         db.create_all()
+        products = db.session.query(Products).all()
+        # Pre-populate DB with some info
+        if not products:
+            objects = [
+                Products(name="Catamaran", list_price=1000),
+                Products(name="Dinghy", list_price=150),
+                Products(name="Narrowboat", list_price=500),
+                Products(name="Submarine", list_price=2000)
+            ]
+            db.session.bulk_save_objects(objects)
+            db.session.commit()
 
         return app
 
